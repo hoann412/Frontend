8c8464a55f86ef93cc3d771a97c091e20bb443d0
CaoChiPhat <122960893+CaoChiPhat2004@users.noreply.github.com>
2025-06-24 22:55:24 +0700

    Create AuthProtected.tsx

---
+++ src/layouts/Protected/AuthProtected.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { MAIN_ROUTES } from '~/constants/router';
+import { useTypedSelector } from '~/store/store';
+
+const AuthProtected = ({ children }: { children: ReactNode }) => {
+    const user = useTypedSelector((state) => state.auth.user);
+    const isAuthenticate = useTypedSelector((state) => state.auth.authenticate);
+    const location = useLocation();
+    const isAuthPage =
+        location.pathname === MAIN_ROUTES.LOGIN ||
+        location.pathname === MAIN_ROUTES.REGISTER ||
+        location.pathname === MAIN_ROUTES.FORGOT_PASSWORD;
+
+    if (isAuthenticate && isAuthPage) {
+        return <Navigate to={'/'} replace />;
+    }
+    if (!user && !isAuthPage) {
+        return <Navigate to={'/'} replace />;
+    }
+    if (user && isAuthPage) {
+        return <Navigate to={'/'} replace />;
+    }
+    return <>{children}</>;
+};
+
+export default AuthProtected;
