bf87245853f67676f63f0940e3f14017e62141cd
CaoChiPhat <122960893+CaoChiPhat2004@users.noreply.github.com>
2025-06-24 22:42:57 +0700

    Add files via upload
    ảnh , font , SVG

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import userImageDf from './img/anhdd.png';
+// import logo from './img/logo.png';
+import frameUser from './img/frameUser.png';
+
+const StaticImages = {
+    userImageDf,
+    // logo,
+    frameUser,
+};
+
+export default StaticImages;
