c43512cbd4264a5257a2a5d6347a787ae80a2479
anhtt1910 <162964361+anhtt1910@users.noreply.github.com>
2025-07-16 12:43:26 +0700

    feature/tuananh-product-ui

--- src/pages/Admin/_product_/Helper/_product_/_component/UploadButton.tsx
+++
@@ -1,8 +0,0 @@
-import { PlusOutlined } from '@ant-design/icons';
-
-export const UploadButton = (
-    <button style={{ border: 0, background: 'none' }} type='button'>
-        <PlusOutlined />
-        <div style={{ marginTop: 8 }}>Upload</div>
-    </button>
-);
--- src/validation/Products/Product.ts
+++
@@ -1,4 +0,0 @@
-// @ antdesign validation message
-export const errorMessage = (message: string) => {
-    return Promise.reject(new Error(message));
-};
